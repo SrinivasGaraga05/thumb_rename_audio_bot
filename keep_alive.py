ce4d61cf3c369cea2e064adda1d305ff53637188
SrinivasGaraga05 <srinivasoffical22@gmail.com>
2025-03-03 19:36:12 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,20 +1,15 @@
 from flask import Flask
 from threading import Thread
 
-# Create a Flask app
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "🤖 Bot is running and alive!"
 
-# Function to run Flask in a separate thread
 def run():
-    port = 8080  # Default port for web hosting
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
 
-# Start Flask in a separate thread
 def keep_alive():
-    server = Thread(target=run)
-    server.daemon = True
-    server.start()
+    thread = Thread(target=run)
+    thread.start()
